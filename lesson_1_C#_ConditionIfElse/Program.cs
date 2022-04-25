969130f288aa6154a307cfe98f46b753f9ad0b04
Sergei_Kravtsov <boyaric@gmail.com>
2022-04-25 22:23:26 +0300

    add Condition If Else

---
+++ lesson_1_C#_ConditionIfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+if(username.ToLower() == "сергей")
+{
+    Console.WriteLine("Ура, это же Сергей!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
