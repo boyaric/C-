5bde7a3fb83cf4b3e6b66017ce4367d280577c62
Sergei_Kravtsov <boyaric@gmail.com>
2022-04-25 22:37:19 +0300

    add lesson_1_C#_Base

---
+++ lesson_1_C#_Base/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 3;
+int b = 7;
+int c = 9;
+int d = 2;
+int e = 1;
+
+int max = a;
+
+if(a>max) max=a;
+if(b>max) max=b;
+if(c>max) max=c;
+if(d>max) max=d;
+if(e>max) max=e;
+
+Console.Write("Максимальное число: ");
+Console.WriteLine(max);
